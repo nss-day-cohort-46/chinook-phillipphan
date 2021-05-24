0005d5b986737adfe1ef24567bdc335fd9a73482
phillipphan <itsphillipphan@gmail.com>
2021-05-24 10:47:27 -0500

    exercise 7

--- chinook-db.session.sql
+++ chinook-db.session.sql
@@ -24,9 +24,20 @@ SELECT DISTINCT BillingCountry
 FROM Invoice
 
 SELECT
-    i.*,
-    c.FirstName,
-    c.LastName
+    e.FirstName,
+    e.LastName,
+    i.*
 FROM Invoice as i
 JOIN Customer as c ON i.CustomerId = c.CustomerId
-JOIN Employee as e on c.SupportRepId = e.
+JOIN Employee as e on c.SupportRepId = e.EmployeeId
+
+SELECT 
+    c.FirstName,
+    c.LastName,
+    c.Country,
+    e.FirstName as AgentFirstName,
+    e.LastName as AgentLastName,
+    i.Total
+FROM Customer as c
+JOIN Employee as e ON c.SupportRepId = e.EmployeeId
+JOIN Invoice as i ON c.CustomerId = i.CustomerId
