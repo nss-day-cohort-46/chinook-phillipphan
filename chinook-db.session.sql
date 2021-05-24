c0c3f01d3f9afdaf7496661047e8f2232a6957a7
phillipphan <itsphillipphan@gmail.com>
2021-05-24 10:13:54 -0500

    exercise 3

--- chinook-db.session.sql
+++ chinook-db.session.sql
@@ -5,3 +5,13 @@ WHERE Country IS NOT "USA"
 SELECT FirstName, LastName, CustomerId, Country
 FROM Customer
 WHERE Country IS "Brazil"
+
+SELECT 
+    FirstName, 
+    LastName, 
+    InvoiceId,
+    InvoiceDate,
+    BillingCountry
+FROM Customer
+JOIN Invoice
+WHERE Country IS "Brazil"
