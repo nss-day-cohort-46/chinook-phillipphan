a3d688f29172efc7b1aca33448b1055636dcabbf
phillipphan <itsphillipphan@gmail.com>
2021-05-24 10:54:15 -0500

    exercise 8

--- chinook-db.session.sql
+++ chinook-db.session.sql
@@ -1,11 +1,14 @@
+-- non_usa_customers.sql
 SELECT FirstName, LastName, CustomerId, Country
 FROM Customer
 WHERE Country IS NOT "USA"
 
+-- brazil_customers.sql
 SELECT FirstName, LastName, CustomerId, Country
 FROM Customer
 WHERE Country IS "Brazil"
 
+-- brazil_customers_invoices.sql
 SELECT 
     FirstName, 
     LastName, 
@@ -16,13 +19,16 @@ FROM Customer
 JOIN Invoice
 WHERE Country IS "Brazil"
 
+-- sales_agents.sql
 SELECT *
 FROM Employee
 WHERE Title IS "Sales Support Agent"
 
+-- unique_invoice_countries.sql
 SELECT DISTINCT BillingCountry
 FROM Invoice
 
+-- sales_agent_invoices.sql
 SELECT
     e.FirstName,
     e.LastName,
@@ -31,6 +37,7 @@ FROM Invoice as i
 JOIN Customer as c ON i.CustomerId = c.CustomerId
 JOIN Employee as e on c.SupportRepId = e.EmployeeId
 
+-- invoice_totals.sql
 SELECT 
     c.FirstName,
     c.LastName,
@@ -41,3 +48,8 @@ SELECT
 FROM Customer as c
 JOIN Employee as e ON c.SupportRepId = e.EmployeeId
 JOIN Invoice as i ON c.CustomerId = i.CustomerId
+
+-- total_invoices_{year}.sql
+SELECT COUNT(InvoiceId)
+FROM Invoice
+WHERE InvoiceDate LIKE "2009%" OR InvoiceDate LIKE "2011%"
