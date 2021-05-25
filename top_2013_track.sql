dfd666ae1a52c910625d424186d99cc5d7923435
phillipphan <itsphillipphan@gmail.com>
2021-05-25 13:18:00 -0500

    exercise 24

---
+++ top_2013_track.sql
@@ -0,0 +1,9 @@
+SELECT
+    COUNT(*) AS TracksSold,
+    t.*
+FROM Invoice AS i
+JOIN InvoiceLine AS il ON i.InvoiceId = il.Invoiceid
+JOIN Track AS t ON t.TrackId = il.TrackId
+WHERE i.InvoiceDate LIKE "2013%"
+GROUP BY il.InvoiceId
+ORDER BY TracksSold DESC
